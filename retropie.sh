038153ee70f949d707a6a31cc03f9b4a3093d864
simonlourson <spam_simonlourson@live.fr>
2017-07-29 20:22:59 +0200

    Create retropie.sh

---
+++ retropie.sh
@@ -0,0 +1,9 @@
+# Manette XBox One bluetooth :
+echo 1 > /sys/module/bluetooth/parameters/disable_ertm
+
+OU
+
+cd /sys/module/bluetooth/parameters/
+sudo nano disable_ertm
+
+/opt/retropie/configs/all/
